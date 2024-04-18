81ac36b1293bdb9e275355caf447428511dab30b
IshaaBari <isha.bari579@gmail.com>
2024-04-18 21:29:53 +0500

    components radio and checkbox

--- src/frontend/bootstrap/checkbox.jsx
+++ src/frontend/bootstrap/checkbox.jsx
@@ -1,22 +1,24 @@
+import Form from "react-bootstrap/Form";
+import React from "react";
 const Checkbox = ({ field, FieldObj, onFieldChange }) => {
   return (
     <>
-      {FieldObj.label()}
+      <Form.Label>
+        {FieldObj.title()}
+        {FieldObj.required() && <span className="text-danger">*</span>}
+      </Form.Label>
       {field.options.map((option) => (
-        <label key={option.option_id}>
-          <input
-            type="checkbox"
-            name={FieldObj.name()}
-            id={option.option_id}
-            className={FieldObj.class()}
-            onChange={(e) => onFieldChange(e, field)}
-            value={option.label}
-            {...FieldObj.input_attributes()}
-          />
-          {option.label}
-        </label>
+        <Form.Check
+          key={option.option_id}
+          type="checkbox"
+          name={FieldObj.name()}
+          id={option.option_id}
+          label={option.label}
+          className={FieldObj.class()}
+          value={option.label}
+          onChange={(e) => onFieldChange(e, field)}
+        />
       ))}
-      <small>{field.description}</small>
     </>
   );
 };
--- src/frontend/bootstrap/radio.jsx
+++ src/frontend/bootstrap/radio.jsx
@@ -1,31 +1,24 @@
 import Form from "react-bootstrap/Form";
+import React from "react";
 const Radio = ({ field, FieldObj, onFieldChange }) => {
   return (
     <>
-      {/* {FieldObj.label()}
+      <Form.Label>
+        {FieldObj.title()}
+        {FieldObj.required() && <span className="text-danger">*</span>}
+      </Form.Label>
       {field.options.map((option) => (
-        <label key={option.option_id}>
-          <input
-            type="radio"
-            name={FieldObj.name()}
-            id={FieldObj.id()}
-            className={FieldObj.class()}
-            onChange={(e) => onFieldChange(e, field)}
-            value={option.label}
-            {...FieldObj.input_attributes()}
-          />
-          {option.label}
-        </label>
+        <Form.Check
+          key={option.option_id}
+          type="radio"
+          name={FieldObj.name()}
+          id={option.option_id}
+          label={option.label}
+          className={FieldObj.class()}
+          value={option.label}
+          onChange={(e) => onFieldChange(e, field)}
+        />
       ))}
-      <small>{field.description}</small> */}
-      <Form.Check // prettier-ignore
-        type="radio"
-        id={FieldObj.id()}
-        label={option.option_id}
-        className={FieldObj.class()}
-        value={option.label}
-        onChange={(e) => onFieldChange(e, field)}
-      />
     </>
   );
 };
